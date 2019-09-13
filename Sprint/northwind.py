3f589f50e1c09232ca8ef946348419961cc7cebe
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2019-09-13 10:08:12 -0600

    touch ups

--- Sprint/northwind.py
+++ Sprint/northwind.py
@@ -1,10 +1,11 @@
 import sqlite3 
 
 # Make Connection 
-conn = sqlite3.connect('northwind_small.sqlite3')
+conn = sqlite3.connect('northwind_small (1).sqlite3')
 # Make Cursor 
 curs = conn.cursor()
 
+'''
 curs.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY
 name;").fetchall()
 [('Category',), ('Customer',), ('CustomerCustomerDemo',),
@@ -19,6 +20,7 @@ curs.execute('SELECT sql FROM sqlite_master WHERE name="Customer";').fetchall()
 VARCHAR(8000) NULL, \n  "Region" VARCHAR(8000) NULL, \n  "PostalCode"
 VARCHAR(8000) NULL, \n  "Country" VARCHAR(8000) NULL, \n  "Phone" VARCHAR(8000)
 NULL, \n  "Fax" VARCHAR(8000) NULL \n)',)]
+'''
 
 # Queries
 query = '''SELECT ProductName, UnitPrice
@@ -60,3 +62,33 @@ GROUP BY CategoryId
 LIMIT 1'''
 ans = curs.execute(query)
 print('LArgest Category by Distinct Products, Limit 1 :', ans)
+
+# Stretch Goal
+#query = '''
+#'''
+#ans = curs.execute(query)
+#print(' :', ans)
+
+
+
+
+'''
+------OPEN ENDED QUESTIONS -------
+Question 1:
+The 'Employee' and 'Territory' Tables Are relational in nature. In the territory table there is a column named 'EmployeeID'.
+This relates back to the 'Employee' table in that that for each indivual 'EmployeeId' in the 'Territory' table, there is detailed
+information about that employee in the 'Employee' table. 
+
+Question 2:
+You would use something like MongoDB when you need something that is high in availability and high in ability to scale. 
+Some examples would be a product catalog and storing the data for blogs.
+It would be BAD to use it in a transactional situation, say storing data at a bank. Ie someone could make a withdrawel, 
+then another withdrawel even if they didn't actually have the money for it but the database simply didn't have the latest data
+available yet. 
+
+Question 3:
+NewSQL attempts to solve the problems that SQL and NoSql could not solve on their own. Primarily in keeping true to the ACID principles, 
+and scalability. Take for example the answer in question2, NewSQL attempts to solve the problem by being both highly scalable, AND able to 
+have the latest data readily available as quickly as possible to avoid the issue that happens in the bank account withdrawel example. 
+
+'''
