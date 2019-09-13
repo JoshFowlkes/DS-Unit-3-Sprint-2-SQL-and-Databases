afff8a1653fee3a1f3153320b8787f8d6c56352e
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2019-09-13 09:07:52 -0600

    adding initial stuff

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import sqlite3
-from sqlalchemy import create_engine
 
 conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 curs = conn.cursor()
 
 df = pd.read_csv('buddymove_holidayiq.csv')
 df.to_sql('review', conn, if_exists='replace', index_label='id')
+
+query = 'SELECT * FROM review'
+df = pd.read_sql(query, conn)
+print(df)
 
 query = "SELECT * FROM review"
 conn.execute(query)
@@ -18,12 +21,7 @@ print('Rows:', results.fetchall()[0][0])
 
 query = """SELECT COUNT(*)
 FROM review
-WHERE Nature >=100 AND Shopping >=100
+WHERE (Nature >=100) AND (Shopping >=100)
 """
 results - conn.execute(query)
 print("More than 100 nature and shopping reviews: ", results.fetchall()[0][0])
-
-
-
-
-
